fc697155baff219e571fdb4db50a386e1ced0591
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2018-11-25 18:17:22 -0200

    Birectional relationship

--- server/src/main/java/com/globalweb/springapp/model/Company.java
+++ server/src/main/java/com/globalweb/springapp/model/Company.java
@@ -1,16 +1,16 @@
 package com.globalweb.springapp.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -19,5 +19,12 @@ public class Company extends AbstractEntity {
     @Column
     @NotNull
     private String name;
+
+    @OneToMany(mappedBy = "company")
+    private List<Product> products;
+
+    public Company(String name) {
+        this.name = name;
+    }
 
 }
