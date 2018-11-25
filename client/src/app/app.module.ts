8e76537a837b02db3f8ee4a634426bbfd8e07e50
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2018-11-25 15:10:20 -0200

    Adding company service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { HttpModule } from '@angular/http';
+import { ConfigService } from './services/config.service';
+import { CompanyService } from './services/company.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ConfigService, CompanyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/services/company.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { ConfigService } from "./config.service";
+import { Company } from "../model/company";
+
+import { map } from 'rxjs/operators'; // Angular 6
+
+@Injectable()
+export class CompanyService {
+
+    private baseUrlService: string = '';
+    private headers: Headers;
+    private options: RequestOptions;
+
+    constructor(private http: Http,
+                private configService: ConfigService) {
+
+        this.baseUrlService = configService.getUrlService() + '/companies/';
+        this.headers = new Headers({'Content-Type': 'application/json;charset=utf-8'});
+        this.options = new RequestOptions({headers: this.headers});
+    }
+
+    getCompanies() {
+        return this.http.get(this.baseUrlService)
+            .pipe(map(res => res.json()));
+    }
+
+    addCompany(company: Company) {
+        return this.http.post(this.baseUrlService, JSON.stringify(company), this.options)
+            .pipe(map(res => res.json()));
+    }
+
+    editCompany(company: Company) {
+        return this.http.put(this.baseUrlService, JSON.stringify(company), this.options)
+            .pipe(map(res => res.json()));
+    }
+
+    deleteCompany(companyId: number) {
+        return this.http.delete(this.baseUrlService + companyId)
+            .pipe(map(res => res.json()));
+    }
+
+    getCompany(companyId: number) {
+        return this.http.get(this.baseUrlService + companyId)
+            .pipe(map(res => res.json()));
+    }
+
+}
