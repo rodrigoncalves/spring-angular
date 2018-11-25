5b76162b93fc783044267059d7c65e56bc548741
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2018-11-25 15:55:54 -0200

    Company list

--- client/src/app/company/company.component.ts
+++ client/src/app/company/company.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from '../model/company';
+import { CompanyService } from '../services/company.service';
+import { Router } from '@angular/router';
+import { ServerResponse } from '../services/server.response';
 
 @Component({
-  selector: 'app-company',
-  templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+    selector: 'app-company',
+    templateUrl: './company.component.html',
+    styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
 
-  constructor() { }
+    private companies: Company[] = new Array();
+    private title: string;
 
-  ngOnInit() {
-  }
+    constructor(private companyService: CompanyService,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+        this.title = 'Lista de empresas';
+        this.companyService.getCompanies()
+            .subscribe(res => this.companies = res);
+    }
+
+    edit(id: number): void {
+        this.router.navigate(['/companies/form', id])
+    }
+
+    delete(id: number, index: number): void {
+        if (confirm("Deseja realmente excluir este registro?")) {
+            this.companyService.deleteCompany(id).subscribe(response => {
+                let res: ServerResponse = <ServerResponse>response;
+
+                alert(res.message);
+
+                if (res.code == 1) { // success
+                    // remove row from table
+                    this.companies.splice(index, 1);
+                }
+            }, error => alert(error));
+        }
+    }
 
 }
